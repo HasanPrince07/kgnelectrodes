50cd65ba14ac8059dba22e86d7684af1fe3b4de2
Mohammad Hasan <131869132+HasanPrince07@users.noreply.github.com>
2026-05-02 14:23:01 +0530

    Delete backend directory

--- backend/model/mainimage.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const mainimageSchema = mongoose.Schema({
-    image: String,
-})
-
-module.exports = mongoose.model('mainimage', mainimageSchema)
--- backend/model/testi.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const testiSchema = mongoose.Schema({
-    name: String,
-    desc: String,
-    status: { type: String, default: 'unpublish' }
-})
-
-module.exports = mongoose.model('testi', testiSchema)
